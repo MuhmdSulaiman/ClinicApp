6f1d4d2d1d6953943cc8cd9e4743ead0d6f90741
MuhmdSulaiman <Sulaimanmohammd34@gmail.com>
2026-02-20 00:55:34 +0530

    as

--- clinic-project/app.js
+++ clinic-project/app.js
@@ -55,4 +55,11 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
+
+
 module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
