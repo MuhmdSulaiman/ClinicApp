36e3c0c58150b9e3d67e575831064a386623f2a1
MuhmdSulaiman <Sulaimanmohammd34@gmail.com>
2026-02-20 00:59:04 +0530

    dv

--- clinic-project/app.js
+++ clinic-project/app.js
@@ -34,6 +34,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.send('Clinic API is running 🚀');
+});
 
 // API routes 
               
