a51baaf9a43cbc425ac7bc29c5349f562c2f12f5
MuhmdSulaiman <Sulaimanmohammd34@gmail.com>
2026-02-20 21:55:35 +0530

    login

--- clinic-project-frontend/src/user/LoginForm.js
+++ clinic-project-frontend/src/user/LoginForm.js
@@ -27,7 +27,7 @@
       setError('');
 
       try {
-        const response = await axios.post('https://clinicapp-1-rloo.onrender.com', formData);
+        const response = await axios.post('https://clinicapp-1-rloo.onrender.com//users/login', formData);
         setToken(response.data.token);
         dispatch(setUser({ user: response.data.user, token: response.data.token }));
         // localStorage.setItem('token', response.data.token);
