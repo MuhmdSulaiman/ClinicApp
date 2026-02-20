a62aacf9b66ba55d8c7e5f0c7af961fa9ad442f5
MuhmdSulaiman <Sulaimanmohammd34@gmail.com>
2026-02-20 22:08:57 +0530

    signup

--- clinic-project-frontend/src/user/SignupForm.js
+++ clinic-project-frontend/src/user/SignupForm.js
@@ -26,7 +26,7 @@ const SignupForm = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('https://clinicapp-1-rloo.onrender.com/users/register', formData);
+      const res = await axios.post('https://clinicapp-1-rloo.onrender.com/users/signup', formData);
       setMessage(res.data.message);
     } catch (err) {
       console.error(err);
