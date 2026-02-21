1176b3ae1df72a1ccfc9cdb069d731d552b58e2d
MuhmdSulaiman <Sulaimanmohammd34@gmail.com>
2026-02-22 02:22:30 +0530

    rm

--- clinic-project/routes/doctors.js
+++ clinic-project/routes/doctors.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { requireRole } = require('../middleware/roleMiddleware');
-const verifyToken = require('./middleware/authMiddleware');
+const verifyToken = require('../middleware/authmiddleware');
 
 const {
   getDoctors,
